15ef08b4000c54ef044509b6457339b54e7ec584
shivprogrammer <shiv.unadkat@gmail.com>
2017-12-08 04:45:45 -0800

    finished off create user

--- functions/create_user.js
+++ functions/create_user.js
@@ -9,7 +9,11 @@ module.exports = function(req, res) {
   // Format the phone number to remove dashes and parens
   const phone = String(req.body.phone).replace(/[^\d]/g, "");
 
-  // Create a new user account using that phone nunber
+  // Create a new user account using that phone number
+  admin.auth().createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({ error: err }));
 
   // Respond to the user request, saying the account was made
+
 }
